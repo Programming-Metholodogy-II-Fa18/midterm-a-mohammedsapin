ae7d00283ab9cc990d2ff33d54997a8b719ff14b
Mohammed Sapin <sapin.mohammed@gmail.com>
2018-11-02 18:41:40 -0400

    problem 3

---
+++ main3.java
@@ -0,0 +1,65 @@
+public class main {
+
+	public static void main(String[] args) {
+		Data object = new Data();
+		object.add(1);
+		object.add(9);
+		object.add(4);
+		object.add(5);
+		object.add(10);
+		object.add(0);
+		
+	}
+	public static class Data{
+		private Node first;
+		public int size;
+		public int index;
+		public class Node{
+			private int data;
+			private Node next;
+		}
+		
+		public Data(){
+			first = null;
+		}
+		
+		public boolean isEmpty(){
+			return first == null;
+		}
+		
+		public void add(int data){
+			Node head = first;
+			first = new Node();
+			first.data = data;
+			first.next = head;
+			size++;
+		}
+		
+		public int peek(){
+			return first.data;
+		}
+		
+		public int remove(){
+			int data = first.data;
+			first = first.next;
+			size--;
+			return data;
+		}
+		
+		public int getValue(int index){
+			int data = 0;
+			
+			if (index > size){
+				return -1;
+			}
+			else{
+				for(int i = 0;i<=index;i++){
+					data = first.data;
+					first = first.next;
+					}
+				add(data);
+				return data;
+			}
+			}
+		}
+	}
