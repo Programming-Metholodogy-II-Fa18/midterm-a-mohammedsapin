44bc46e62dc6ecd3b5345afb220d192a571b7f1d
Mohammed Sapin <sapin.mohammed@gmail.com>
2018-11-02 18:40:23 -0400

    Problem 2

--- main.java
+++ main.java
@@ -1,47 +1,71 @@
+
 public class main {
 
 	public static void main(String[] args) {
-		int[] a = {15,9,60,44,12,1,4};
-		int size = a.length;
-		mergeSort(a,size);
-		for(int i:a){
-			System.out.println(i+" ");
-		}
+		Data object = new Data();
+		object.add(1);
+		object.add(9);
+		object.add(4);
+		object.add(5);
+		object.add(10);
+		object.add(0);
+		System.out.println(object.getValue(0));
+		System.out.println(object.getValue(3));
+
 	}
-	
-	public static void merge(int[] a, int[] left, int[]right, int leftSide, int rightSide){
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while(i < leftSide && j < rightSide){
-			if(left[i] < right[j]){
-				a[k++] = left[i++];
+	public static class Data{
+		private Node first;
+		public int size;
+		public int index;
+		public class Node{
+			private int data;
+			private Node next;
+		}
+		
+		public Data(){
+			first = null;
+		}
+		
+		public boolean isEmpty(){
+			return first == null;
+		}
+		
+		public void add(int data){
+			Node head = first;
+			first = new Node();
+			first.data = data;
+			first.next = head;
+			size++;
+		}
+		
+		public int peek(){
+			return first.data;
+		}
+		public int size(){
+			return size;
+		}
+		
+		public int remove(){
+			int data = first.data;
+			first = first.next;
+			size--;
+			return data;
+		}
+		
+		public int getValue(int index){
+			int data = 0;
+			
+			if (index > size){
+				return -1;
 			}
 			else{
-				a[k++] = right[j++];
+				for(int i = 0;i<=index;i++){
+					data = first.data;
+					first = first.next;
+					}
+				add(data);
+				return data;
+			}
 			}
 		}
-		while(i < leftSide){
-			a[k++] = left[i++];
-		}
-		while(j < rightSide){
-			a[k++] = right[j++];
-		}
 	}
-	public static void mergeSort(int[]a, int size){
-		if(size < 2) return;
-		int mid = size/2;
-		int[] left = new int[mid];
-		int[] right = new int[size - mid];
-		
-		for(int i = 0; i<mid; i++){
-			left[i] = a[i];
-		}
-		for(int i = 0; i<mid; i++){
-			right[i] = a[i+mid];
-		}
-		mergeSort(left,mid);
-		mergeSort(right,size-mid);
-		merge(a,left,right,mid,size-mid);
-	}
-}
